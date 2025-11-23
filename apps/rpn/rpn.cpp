34a7b82694169802cc92641170e5cdd3a13e33b3
dongkim_wsl <parktest0325@gmail.com>
2025-11-23 15:29:13 +0900

    call kernel function in userapp(rpn)

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,5 +1,12 @@
 #include <cstring>
 #include <cstdlib>
+#include "../../kernel/graphics.hpp"
+#include "../../kernel/font.hpp"
+
+auto& printk = *reinterpret_cast<int (*)(const char*, ...)>(0x00000000001108e0);
+auto& fill_rect = *reinterpret_cast<decltype(FillRectangle)*>(0x0000000000148e10);
+auto& write_str = *reinterpret_cast<decltype(WriteString)*>(0x0000000000148d30);
+auto& scrn_writer = *reinterpret_cast<decltype(screen_writer)*>(0x00000000002630c8);
 
 int stack_ptr;
 long stack[100];
@@ -21,17 +28,25 @@ extern "C" int main(int argc, char** argv) {
       long b = Pop();
       long a = Pop();
       Push(a + b);
+      printk("[%d] <- %ld\n", stack_ptr, a + b);
     } else if (strcmp(argv[i], "-") == 0) {
       long b = Pop();
       long a = Pop();
       Push(a - b);
+      printk("[%d] <- %ld\n", stack_ptr, a - b);
     } else {
       long a = atol(argv[i]);
       Push(a);
+      printk("[%d] <- %ld\n", stack_ptr, a);
     }
   }
+
+  fill_rect(*scrn_writer, Vector2D<int>{100, 10}, Vector2D<int>{200, 200}, ToColor(0x66ff66));
+  write_str(*scrn_writer, Vector2D<int>{100 + 50, 10 + 50}, "Hello RPN World!", ToColor(0xff66ff));
   if (stack_ptr < 0) {
     return 0;
   }
+
+
   return static_cast<int>(Pop());
 }
