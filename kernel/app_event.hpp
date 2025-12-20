acfbd8d031a19143fac7cc0a6b704fdcb35224f4
dongkim_wsl <parktest0325@gmail.com>
2025-12-21 02:00:38 +0900

    SyscallReadEvent MouseEvent. eyes app

--- kernel/app_event.hpp
+++ kernel/app_event.hpp
@@ -7,8 +7,18 @@ extern "C" {
 struct AppEvent {
   enum Type {
     kQuit,
+    kMouseMove,
   } type;
+
+  union {
+    struct {
+      int x, y;
+      int dx, dy;
+      uint8_t buttons;
+    } mouse_move;
+  } arg;
 };
+
 
 #ifdef __cplusplus
 } // extern "C"
